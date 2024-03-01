98a0455c4a52a2edb45adc85b196cab8074dc3ea
Priya Ranjan Kumar <ranjanrana1712@gmail.com>
2024-03-01 06:09:56 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>SOCIAL MEDIA ICON WITH POP UP</h1>
+      <main>
+        <ul>
+          <li>
+            <a href="">Github</a>
+          </li>
+
+          <li>
+            <a href="">Facebook</a>
+          </li>
+          <li>
+            <a href="">Instagram</a>
+          </li>
+          <li>
+            <a href="">X</a>
+          </li>
+          <li>
+            <a href="">Youtube</a>
+          </li>
+        </ul>
+      </main>
+    </div>
+  );
+}
